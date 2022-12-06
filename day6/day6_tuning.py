5c256e2da7fc378677413ad8736ff351f322c064
Zeno Vandenbulcke <zeno.vandenbulcke@student.kuleuven.be>
2022-12-06 12:29:24 +0100

    day 6 done

--- day6/day6_tuning.py
+++ day6/day6_tuning.py
@@ -1,10 +1,13 @@
 def main():
     string = open("input.txt").readline()
+
+    #output 1
     for i in range(len(string)-4):
         if len(set(string[i:i+4])) == 4:
             print(i + 4)
             break
 
+    #output 2
     for i in range(len(string)-14):
         if len(set(string[i:i+14])) == 14:
             print(i + 14)
